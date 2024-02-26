8af590cf07186f9c1d740638c1a245d9eafb5e5b
P K Navin Shrinivas <karupal2002@gmail.com>
2024-02-26 10:08:56 +0530

    ok

---
+++ src/kotty/FirstClass.kt
@@ -0,0 +1,15 @@
+package kotty
+
+class FirstClass {
+}
+
+fun main(){
+    var x = "123"; // [TODO]
+//    x = 2; //[ERROR] Strong type but dynamic typing
+    println("Hello, World!") // Simple syntax
+    var ke = KotlinEmployee(
+            "abc",
+            2,
+            "xyz"
+    );
+}
---
+++ src/kotty/KotlinEmployee.kt
@@ -0,0 +1,7 @@
+package kotty
+
+class KotlinEmployee(name: Any?, age: Any?, dept: Any?) {
+    var name: String? = null
+    var age: Int = 0
+    var dept: String? = null
+}
